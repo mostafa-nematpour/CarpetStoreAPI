888806f463df9b54b052689a608cbf0d266a1946
Mostafa <nematpor46@gmail.com>
2022-04-08 14:13:09 +0330

    add ProductFactory and seed

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'short_name' => "فرش",
+            'name' => $this->faker->name(),
+            'price' => $this->faker->randomNumber(4)*1000,
+            'number' => $this->faker->randomNumber(2),
+            'thumbnail' => 'https://loremflickr.com/446/240?random='.rand(1, 99),
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->text(),
+            'material' => $this->faker->name(),
+        ];
+    }
+}
