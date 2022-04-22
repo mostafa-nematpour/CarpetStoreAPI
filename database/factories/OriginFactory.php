f3b42c0a3e84dbc98badd89b72168404d3d7717e
Mostafa <nematpor46@gmail.com>
2022-04-22 10:53:56 +0330

    origin factory bug fix

--- database/factories/OriginFactory.php
+++ database/factories/OriginFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class OriginsFactory extends Factory
+class OriginFactory extends Factory
 {
     /**
      * Define the model's default state.
