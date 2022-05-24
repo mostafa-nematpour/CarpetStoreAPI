7fe744f07a083f2a1f7134b973e42407ea97e2b1
Mostafa <nematpor46@gmail.com>
2022-05-24 14:38:50 +0330

    Add products sort

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,38 +7,75 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // $products = Product::select('short_name', 'name', 'slug', 'price', 'description', 'discount')->get();
 
-        // $products = Product::with('category', 'origin')->get();
+
         $products = Product::with([
-            'category'=> function ($query) {
+            'category' => function ($query) {
                 $query->select('id', 'name', 'slug');
             },
-            'origin'=> function ($query) {
+            'origin' => function ($query) {
                 $query->select('id', 'name', 'slug');
-            }])
+            }
+        ])
             ->get([
-                'id','short_name', 'name', 'slug', 'price', 'description', 'discount' ,'category_id', 'origin_id'
+                'id', 'short_name', 'name', 'slug', 'price', 'description', 'discount', 'category_id', 'origin_id'
             ]);
 
         $products->makeHidden(['category_id', 'origin_id']);
 
+
+
+        if ($request->has('sort')) {
+            switch ($request->sort) {
+                case 'price':
+                    $products = $products->sortBy('price');
+                    break;
+                case 'price-desc':
+                    $products = $products->sortByDesc('price');
+                    break;
+                case 'newest':
+                    $products = $products->sortByDesc('created_at');
+                    break;
+                case 'older':
+                    $products = $products->sortBy('created_at');
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if ($request->has('available') && $request->available == 'true') {
+            // dd($products);
+            // $products = $products->where('number', '>', 0);
+            $products = $products->where('number', '!=', 0);
+        }
+
+
+        // $products = Product::select('short_name', 'name', 'slug', 'price', 'description', 'discount')->get();
+
+        // $products = Product::with('category', 'origin')->get();
+
         return response()->json($products);
     }
+
+
 
     public function show(Product $product)
     {
         $product = $product->load([
-            'category'=> function ($query) {
+            'category' => function ($query) {
                 $query->select('id', 'name', 'slug');
             },
-            'origin'=> function ($query) {
+            'origin' => function ($query) {
                 $query->select('id', 'name', 'slug');
             }
         ]);
-    
+
         return $product;
     }
+
+
+
 }
